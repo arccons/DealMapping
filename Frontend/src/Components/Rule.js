81f817d6f6cb63dfad7ae58e27a21960b09ed9c8
arccons <46444032+arccons@users.noreply.github.com>
2024-06-15 06:19:22 +0530

    Rule coloring added

--- Frontend/src/Components/Rule.js
+++ Frontend/src/Components/Rule.js
@@ -1,10 +1,16 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import './Rule.css'
 
 export default function Rule({ parsedFile, rule, result }) {
+   const [ruleColor, setRuleColor] = useState();
+
+   useEffect(() => {
+      result.length === 0 ? setRuleColor('passed-rule') : setRuleColor('failed-rule')
+   }, [result.length])
 
    return (
       <>
-         <h6>{rule}</h6>
+         <h6 className={ruleColor}>{rule}</h6>
          {result.map((rs, rsIndex) => {
             return <div key={rsIndex}><p>{parsedFile[rs].toString()}</p></div>
          })}
